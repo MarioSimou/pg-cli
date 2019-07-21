8d8188c3787ad3104872d21dd0ccfc70906c321f
MarioSimou <marios.simouu@gmail.com>
2019-07-21 13:36:15 +0100

    initial commit

---
+++ src/Types/SqlString.js
@@ -0,0 +1,52 @@
+import { Operators } from './constants'
+
+class SqlString extends String {
+    constructor(string){
+        super()
+        this._init = true
+        this._sql = string || ''
+    }
+    // settes/getters - Encapsulation
+    get sql(){
+        return this._sql
+    }
+    get and(){
+        this._sql += this._sep(Operators.AND)
+        console.log(this._sql)
+        return this
+    }
+    get or(){
+        this._sql += this._sep(Operators.OR)
+        return this
+    }
+    // public methods
+    equals( v , k = null ){
+        this._appendSql(k,v,Operators.EQUAL)
+        return this 
+    }
+    unequal( v , k = null ){
+        this._appendSql(k,v,Operators.UNEQUAL)
+        return this 
+    }
+    // private methods
+    _sep(sep){
+        return ` ${sep} `
+    }
+    _appendSql(...args){
+        const [ k , v , operator ] = args
+        switch(k){
+            case null:
+                const sql = this._sql
+                this._sql = `${sql}${operator}'${v}'`
+                break;
+            default:
+                this._sql += `${k}${operator}'${v}'`
+                break;
+        }
+
+        this._init = false
+        return this._sql
+    }
+}
+
+export default SqlString
---
+++ src/Types/constants.js
@@ -0,0 +1,12 @@
+export const Operators = {
+    'OR':'OR',
+    'AND':'AND',
+    'ANY':'ANY',
+    'ALL':'ALL',
+    'EQUAL': '=',
+    'UNEQUAL': '<>'
+}
+
+export const CONTANTS = {
+    Operators: Operators
+}
